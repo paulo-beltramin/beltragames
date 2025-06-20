a550b5bee1a67d1308b3f2b4a8ad609d7e8e3349
paulo-beltramin <dev.beltramin@gmail.com>
2025-06-19 23:09:58 -0300

    atualzado nome do logo no header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,7 +7,7 @@ const Header = () => {
             <header className='flex justify-between max-w-5xl mx-auto h-28 items-center w-11/12 lg:w-full'>
                 <div className='flex items-center '>
                     <p className='text-2xl font-bold lg:text-3xl'>
-                        Daly<span className='text-amber-600'>Games</span>
+                        Beltra<span className='text-amber-600'>Games</span>
                     </p>
 
                     <ul className='flex pl-4 gap-6'>
@@ -21,9 +21,7 @@ const Header = () => {
                     </ul>
                 </div>
 
-                <div className='border rounded-full p-1 cursor-pointer hover:text-blue-950'>
-                    <FaRegUser size={24} />
-                </div>
+
             </header>
         </div>
     )
